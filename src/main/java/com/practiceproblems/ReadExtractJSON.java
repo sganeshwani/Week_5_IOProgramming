4e26e3a164adf3ce87f3b230910231f9acc5a05b
sganeshwani <ganeshwani2003@gmail.com>
2025-02-19 16:47:58 +0530

    [Sahil Ganeshwani] Add . Read and extract name email from JSON file

---
+++ src/main/java/com/practiceproblems/ReadExtractJSON.java
@@ -0,0 +1,40 @@
+package com.tit.json.practiceproblems;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadExtractJSON {
+    public static void main(String[] args) {
+        // Path to the JSON file
+        String filePath = "C:\\Users\\HP\\Desktop\\Capgemini Training\\Week5_IOProgramming\\Day05_JSONData\\src\\main\\resources\\user.json";
+
+        // StringBuilder to hold the content of the JSON file
+        StringBuilder jsonContent = new StringBuilder();
+
+        // Reading the JSON file
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonContent.append(line);
+            }
+        } catch (IOException io) {
+            System.out.println(io.getMessage());
+        }
+
+        // Creating a JSONArray from the file content
+        JSONArray jsonObjects = new JSONArray(jsonContent.toString());
+
+        // Iterating through the JSONArray to extract specific fields
+        for (int i = 0; i < jsonObjects.length(); i++) {
+            JSONObject object = jsonObjects.getJSONObject(i);
+            String name = object.getString("name");
+            String email = object.getString("email");
+            System.out.println("Name: " + name);
+            System.out.println("Email: " + email);
+        }
+    }
+}
